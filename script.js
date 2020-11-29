3766f5e68af750f120d2877c768f75d1aaaae330
Magda Castro <magda@gmail.com>
2020-11-28 23:15:16 -0300

    biblioteca css com as funcionalidades principais.

---
+++ script.js
@@ -0,0 +1,73 @@
+const controles = document.getElementById('controles');
+const cssText = document.querySelector('.css');
+const btn = document.querySelector('.btn');
+controles.addEventListener('change', handleChange);
+
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + 'px';
+  },
+  width(value) {
+    this.element.style.width = value + 'px';
+  },
+  textAlign(value) {
+    this.element.style.textAlign = value;
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + 'px';
+  },
+  boxShadow(value) {
+    this.element.style.boxShadow = value;
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + 'rem';
+  },
+  cursor(value) {
+    this.element.style.cursor = value;
+  }
+}
+
+
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  handleStyle[name](value);
+  saveValues(name, value);
+  showCss();
+}
+
+function showCss() {
+  cssText.innerHTML = "<span>" + btn.style.cssText.split('; ').join(';</span><span>');
+}
+
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+  properties.forEach((property) => {
+    handleStyle[property](localStorage[property])
+    controles.elements[property].value = localStorage[property];
+  });
+  showCss();
+}
+
+setValues();
